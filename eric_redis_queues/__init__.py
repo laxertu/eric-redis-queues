1a7dc26a2b7ec7a117d3b77a13714d6acd2428b3
luca <laxertu@gmail.com>
2025-07-30 14:55:37 +0200

    RC

--- eric_redis_queues/__init__.py
+++ eric_redis_queues/__init__.py
@@ -2,18 +2,21 @@ from typing import Iterable
 from pickle import dumps, loads
 
 from redis import Redis
-
-from eric_sse.entities import AbstractChannel
 from eric_sse.exception import NoMessagesException, RepositoryError
 from eric_sse.message import MessageContract
 from eric_sse.queue import Queue
-from eric_sse.connection import Connection, ConnectionRepositoryInterface, ChannelRepositoryInterface, ChannelInterface, ChannelPersistenceMixin
+from eric_sse.connection import Connection, ConnectionRepositoryInterface
 
 _PREFIX = 'eric-redis-queues'
 _PREFIX_QUEUES = f'eric-redis-queues:q'
 _PREFIX_LISTENERS = f'eric-redis-queues:l'
 _PREFIX_CHANNELS = f'eric-redis-queues:c'
-
+"""
+import importlib
+module = importlib.import_module('my_package.my_module')
+my_class = getattr(module, 'MyClass')
+my_instance = my_class()
+"""
 class RedisQueue(Queue):
 
     def __init__(self, listener_id: str, host='127.0.0.1', port=6379, db=0):
@@ -39,36 +42,6 @@ class RedisQueue(Queue):
             self.__client.rpush(f'{_PREFIX_QUEUES}:{self.id}', dumps(msg))
         except Exception as e:
             raise RepositoryError(e)
-
-class RedisChannelRepository(ChannelRepositoryInterface):
-    def __init__(self, host='127.0.0.1', port=6379, db=0):
-        self.__host: str = host
-        self.__port: int = port
-        self.__db: int = db
-        self.__client = Redis(host=host, port=port, db=db)
-
-    def create(self) -> ChannelInterface:
-        pass
-
-    def load(self) -> Iterable[ChannelPersistenceMixin]:
-        for redis_key in self.__client.scan_iter(f"{_PREFIX_CHANNELS}:*"):
-            try:
-                yield loads(self.__client.get(redis_key.decode()))
-            except Exception as e:
-                raise RepositoryError(e)
-
-    def persist(self, channel: ChannelPersistenceMixin):
-        try:
-            self.__client.set(f'{_PREFIX_CHANNELS}:{channel.key}', dumps(channel))
-        except Exception as e:
-            raise RepositoryError(e)
-
-    def delete(self, channel_id: str):
-        try:
-            self.__client.delete(f'{_PREFIX_CHANNELS}:{channel_id}')
-        except Exception as e:
-            raise RepositoryError(e)
-
 
 class RedisConnectionsRepository(ConnectionRepositoryInterface):
     def __init__(self, host='127.0.0.1', port=6379, db=0):

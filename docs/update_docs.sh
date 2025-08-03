3ec8b1f9528c0108407d7b19f231ef10d577211d
luca <laxertu@gmail.com>
2025-08-03 19:04:49 +0200

    vendors setup

--- docs/update_docs.sh
+++ docs/update_docs.sh
@@ -2,7 +2,6 @@
 
 rm -rf _build/html/
 rm -rf _build/markdown/
-rm -rf _build/doctrees/
 
 make markdown
 make html
@@ -12,7 +11,6 @@ rm -rf ../docs_markdown/
 
 cp -rf _build/html/ ../docs_html/
 cp -rf _build/markdown/ ../docs_markdown/
-cp -rf _build/doctrees/ ../doctrees/
 
 # patch
 #cp -rf source/_static ../docs_markdown/_static
